7e3eafa4c0f071ae57f0f3ddb78912875cdc7a02
jackkaiser1 <jackkaiser78@gmail.com>
2026-04-21 14:38:40 -0400

    feat: add custom error classes & add error handler
    add files:
    - errors.ts
    - error-middleware.ts
    
    add custom error classes for client errors
    add error handler
    register error handler at application level

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness";
+import { errorMiddleware } from "./api/middleware/error-middleware";
 
 const app = express();
 const PORT = 8080;
 
-app.get("/api/readiness", async (req, res) => {
-    await handlerReadiness(req, res);
+app.get("/api/readiness", async (req, res, next) => {
+    Promise.resolve(await handlerReadiness(req, res)).catch(next);
 });
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
