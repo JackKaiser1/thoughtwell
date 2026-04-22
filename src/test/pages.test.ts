b9122a7c579c554a90e97ca44cc3c28e1a9e95c6
jackkaiser1 <jackkaiser78@gmail.com>
2026-04-22 12:14:14 -0400

    test(validatePageData): add vitest & add validatePageData test suite
    add dev dependency:
    - vitest
    
    add file:
    - pages.test.ts
    
    add unit tests:
    - validatePageData

---
+++ src/test/pages.test.ts
@@ -0,0 +1,22 @@
+import { expect, test, describe, it } from "vitest"
+import { validatePageData } from "../api/pages";
+import { BadRequestError } from "../api/errors";
+
+
+describe("validatePageData", () => {
+    it("should return abc", () => {
+        expect(validatePageData({ note: "abc" }, 140)).toBe("abc");
+    });
+
+    it("should throw error due to malformed data", () => {
+        // @ts-ignore  -- Simulate malformed user data from request 
+        expect(() => validatePageData({ noe: "123" }, 140)).toThrow();
+    });
+
+    it("should throw error due to character limit", () => {
+        expect(() => validatePageData({ note: "hello world"}, 5)).toThrow();
+    });
+});
+
+
+
