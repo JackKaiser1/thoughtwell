f7cf9d0b7e96c12a56c09679dfb7491dd6655318
jackkaiser1 <jackkaiser78@gmail.com>
2026-04-23 13:02:24 -0400

    fix: pass schema into drizzle connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import postgres from "postgres";
 import { config } from "../config.js";
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema.js";
 
 const conn = postgres(config.dbURL);
-export const db = drizzle(postgres);
+export const db = drizzle(postgres, { schema });
  
