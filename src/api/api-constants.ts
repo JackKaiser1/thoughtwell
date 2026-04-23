744c7d2bb9beba1d40071d02124ef1699bd4d495
jackkaiser1 <jackkaiser78@gmail.com>
2026-04-23 15:47:50 -0400

    feat: add password hashing & add password entropy checking
    add file:
    - src/api/auth/password.ts
    
    add dir:
    - src/api/auth
    
    edit file:
    - src/api/api-constants.ts
    
    add dependency:
    - argon2

--- src/api/api-constants.ts
+++ src/api/api-constants.ts
@@ -1,1 +1,2 @@
 export const CharacterLimit = 140;
+export const PasswordCharMin = 16;
---
+++ src/api/auth/password.ts
@@ -0,0 +1,42 @@
+import argon2 from "argon2";
+import { BadRequestError, UnauthorizedError } from "../errors.js";
+import { PasswordCharMin } from "../api-constants.js";
+
+export async function processPassword(password: string) {   
+    checkPasswordEntropy(password);
+    const hash = await hashPassword(password);
+    
+    return hash;
+}
+
+function checkPasswordEntropy(password: string): void {
+    const length = password.length;
+    if (length < PasswordCharMin) {
+        throw new BadRequestError("Password is too short");
+    }
+    return;
+}
+
+
+export async function hashPassword(password: string): Promise<string> {
+    try {
+        const hash = await argon2.hash(password);
+        return hash;
+    } catch (err) {
+        console.log("Hashing failed");
+        throw new Error("Internal failure");
+    }
+}
+
+export async function verifyHash(hash: string, password: string): Promise<void> {
+    try {
+        if (await argon2.verify(hash, password)) {
+            return;
+        } else {
+            throw new UnauthorizedError("Incorrect password");
+        }
+    } catch (err) {
+        console.log("Hashing failed");
+        throw new Error("Internal failure");
+    }
+}
