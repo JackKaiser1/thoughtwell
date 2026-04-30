22dbf2c97a25b918cde4c3cda04f2c9fe49af124
jackkaiser1 <jackkaiser78@gmail.com>
2026-04-30 15:25:36 -0400

    feat: add various page query functions
    - add unit tests for page query functions

---
+++ src/db/queries/pages.ts
@@ -0,0 +1,35 @@
+import { type PageRecord, pages, users } from "../schema.js";
+import { type dbClient, db} from "../index.js";
+import { eq } from "drizzle-orm";
+
+type PageQuery = Omit<PageRecord, "createdAt" | "updatedAt" | "id" | "isChild">;
+
+export async function createPage(client: dbClient, page: PageQuery) {
+    const [pageRecord] = await client 
+                            .insert(pages)
+                            .values(page)
+                            .returning();
+    return pageRecord;
+}
+
+export async function getPage(client: dbClient, pageId: string) {
+    const [pageRecord] = await client
+                                    .select()
+                                    .from(pages)
+                                    .where(eq(pages.id, pageId));
+    return pageRecord;
+}
+
+export async function getPages(client: dbClient, userId: string) {
+    const pageRecords = await client 
+                                    .select()
+                                    .from(pages)
+                                    .where(eq(pages.userId, userId));
+    return pageRecords;
+}
+
+export async function deletePage(client: dbClient, pageId: string) {
+    await client
+            .delete(pages)
+            .where(eq(pages.id, pageId));
+}
