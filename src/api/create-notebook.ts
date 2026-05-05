2c83e18b191bb146784bb5d722df5276d4e2aef1
jackkaiser1 <jackkaiser78@gmail.com>
2026-05-05 16:09:27 -0400

    fix(handlerCreateNotebook): add guard clause after getUserFromUsername to prevent reading from undefined

--- src/api/create-notebook.ts
+++ src/api/create-notebook.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { NotebookRecord } from "../db/schema.js";
 import { getUserFromUsername } from "../db/queries/users.js";
 import { createNotebook } from "../db/queries/notebooks.js";
-import { BadRequestError } from "./errors.js";
+import { BadRequestError, NotFoundError } from "./errors.js";
 import { db } from "../db/index.js";
 
 type NotebookRequest = {
@@ -19,6 +19,10 @@ export async function handlerCreateNotebook(req: Request, res: Response) {
     }
 
     const userRecord = await getUserFromUsername(db, notebookReq.userName);
+    if (!userRecord) {
+        throw new NotFoundError("User not found");
+    }
+    
     const userId = userRecord.id;
 
     const notebookQuery: NotebookRecord = {
