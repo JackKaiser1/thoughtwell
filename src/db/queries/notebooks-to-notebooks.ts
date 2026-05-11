4d5c001a54513f3d23262c1597bbabafc786da9c
jackkaiser1 <jackkaiser78@gmail.com>
2026-05-11 17:28:43 -0400

    feat: begin work on fetchAllChildren function

--- src/db/queries/notebooks-to-notebooks.ts
+++ src/db/queries/notebooks-to-notebooks.ts
@@ -1,9 +1,14 @@
 import { dbClient } from "../index.js";
 import { eq } from "drizzle-orm";
-import { type NotebooksToNotebooksRecord, notebooksToNotebooks, pages, pagesToNotebooks } from "../schema.js";
+import { type NotebooksToNotebooksRecord, PagesToNotebooksRecord, notebooksToNotebooks, pages, pagesToNotebooks } from "../schema.js";
 import { exceptAll, unionAll } from 'drizzle-orm/pg-core'
 
 export type NotebooksToNotebooksQuery = Omit<NotebooksToNotebooksRecord, "id" | "createdAt" | "updatedAt">;
+
+export type TopLevelChildren = {
+    pageChildren: PagesToNotebooksRecord[],
+    notebookChildren: NotebooksToNotebooksRecord[],
+}
 
 export async function createNotebooksToNotebooks(client: dbClient, queryData: NotebooksToNotebooksQuery) {
     const [notebooksToNotebooksRecord] = await client 
@@ -14,7 +19,7 @@ export async function createNotebooksToNotebooks(client: dbClient, queryData: No
     return notebooksToNotebooksRecord;
 }
 
-export async function getChildren(client: dbClient, notebookId: string) {
+export async function getChildren(client: dbClient, notebookId: string): Promise<TopLevelChildren> {
     const childNotebookRecords = await client 
                                             .select()
                                             .from(pagesToNotebooks)
