57f2d5167485ed5ce1f45948e0db0098bb886981
jackkaiser1 <jackkaiser78@gmail.com>
2026-05-19 11:35:17 -0400

    feat: add refresh token invalidation to login endpoint

--- src/api/refresh-access.ts
+++ src/api/refresh-access.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { BadRequestError } from "./errors.js";
+import { BadRequestError, UnauthorizedError } from "./errors.js";
 import { createJWT, getBearerToken } from "./auth/jwt.js";
 import { getRefreshToken } from "../db/queries/refresh-tokens.js";
 import { db } from "../db/index.js";
@@ -8,23 +8,23 @@ import { config } from "../config.js";
 
 export async function handlerRefresh(req: Request, res: Response) {
     if (!req.headers.authorization) {
-        throw new BadRequestError("must provided refresh token in header");
+        throw new BadRequestError("Must provided refresh token in header");
     }
 
     const token = getBearerToken(req.headers.authorization);
 
     const refreshTokenRecord = await getRefreshToken(db, token);
     if (!refreshTokenRecord) {
-        throw new BadRequestError("refresh token invalid");
+        throw new UnauthorizedError("Refresh token invalid");
     }
 
     if (refreshTokenRecord.revokedAt !== null) {
-        throw new BadRequestError("refresh token has been revoked");
+        throw new UnauthorizedError("Refresh token has been revoked");
     }
 
     const expiresAt = refreshTokenRecord.expiresAt;
     if (new Date() > expiresAt) {
-        throw new BadRequestError("refresh token has expired");
+        throw new UnauthorizedError("refresh token has expired");
     }
 
     const userId = refreshTokenRecord.userId;
