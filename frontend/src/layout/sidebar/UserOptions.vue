15b81d522c6a0a0b59bc44f37bc2d4413a35ae15
jackkaiser1 <jackkaiser78@gmail.com>
2026-05-29 16:40:31 -0400

    feat(frontend): add delete user functionality

--- frontend/src/layout/sidebar/UserOptions.vue
+++ frontend/src/layout/sidebar/UserOptions.vue
@@ -2,23 +2,72 @@
     import { useLoosePageStore } from '@/stores/loose-pages.js';
     import { useSessionStore } from '../../stores/session.js';
     import { useTopLevelNotebooksStore } from '@/stores/top-level-notebooks.js';
+    import { useSelectedPageStore } from '@/stores/selected-pages.js';
+    import { useSelectedNotebookStore } from '@/stores/selected-notebook.js';
+    import { ref } from 'vue';
+    import { serverURL } from '@/constants.js';
+    import { apiErrorHandler, printError } from '@/lib/errorHandler.js';
+    import { useCurrentNotebookStore } from '@/stores/current-notebook.js';
 
     const username: string = sessionStorage.userName;
+    const isClicked = ref(false);
+
+    function deleteUserClicked() {
+        isClicked.value = true;
+    }
+
+    function deleteUserCanceled() {
+        isClicked.value = false;
+    }
 
     function logoutUser() {
         useSessionStore().logoutSession();
         useLoosePageStore().clearLoosePages();
         useTopLevelNotebooksStore().clearTopLevelNotebooks();
-        console.log(sessionStorage);
+        useSelectedPageStore().clearSelectedPages();
+        useSelectedNotebookStore().clearSelectedNotebook();
+        useCurrentNotebookStore().clearCurrentNotebook();
+    }
+
+    async function deleteUser() {
+        const url = `${serverURL}/api/users/${sessionStorage.userId}`;
+
+        try {
+            const response = await fetch(url, {
+                method: "DELETE",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "",
+                    "Authorization": `Bearer ${sessionStorage.accessToken}`,
+                }
+            });
+
+            if (!response.ok) {
+                apiErrorHandler(response);
+                throw new Error;
+            }
+
+            logoutUser();
+
+        } catch (err) {
+            printError(err);
+        }
     }
 
 </script>
 
 <template>
    <div class="userOptionsContainer">
-    <p class="usernameSidebar">{{ username }} </p>
+        <p class="usernameSidebar">{{ username }} </p>
 
-    <button type="button" class="logoutButton" @click="logoutUser">Logout</button>
+        <button type="button" class="logoutButton" @click="logoutUser">Logout</button>
+        <br>
+        <button type="button" @click="deleteUserClicked" v-if="!isClicked">Delete user</button>
+        
+        <div v-else>
+            <button type="button" @click="deleteUser">Confirm</button>
+            <button type="button" @click="deleteUserCanceled">Cancel</button>
+        </div>
    </div>
 </template>
 
