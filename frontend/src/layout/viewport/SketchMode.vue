7ff3ec18752915527ac35a54aed79831427ac3c9
jackkaiser1 <jackkaiser78@gmail.com>
2026-08-12 15:19:29 -0400

    feat(frontend): add brush size buttons for sketching

--- frontend/src/layout/viewport/SketchMode.vue
+++ frontend/src/layout/viewport/SketchMode.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
-    import { onMounted } from 'vue';
+    import { onMounted, useTemplateRef, ref } from 'vue';
     import p5 from "p5";
-    import { useTemplateRef } from 'vue';
 
+    const pointSize = ref(10);
     var drawing: p5;
     const sketchContainer = useTemplateRef<HTMLElement | undefined>('sketch');
 
     const s = (sketch: p5) => {
         sketch.setup = () => {
-            sketch.createCanvas(300, 450);
+            sketch.createCanvas(250, 400);
             sketch.background(255);
         };
 
         sketch.draw = () => {
             sketch.fill(0);
             sketch.noStroke();
-            if (sketch.mouseIsPressed){
-                sketch.circle(sketch.mouseX, sketch.mouseY, 20);
+            if (sketch.mouseIsPressed) {
+                sketch.circle(sketch.mouseX, sketch.mouseY, pointSize.value);
             }
         };
     }
 
-    function clear() {
+    function submit() {
         if (sketchContainer.value){
             drawing.setup();
             drawing.draw();
         }
+    }
+
+    // Make these one function
+    function changePointSize10() {
+        pointSize.value = 10;
+    }
+
+    function changePointSize20() {
+        pointSize.value = 20;
+    }
+
+    function changePointSize30() {
+        pointSize.value = 30;
     }
 
     onMounted(() => {
@@ -36,22 +49,47 @@
 </script>
 
 <template>
-    <div class="sketchContainer">
-        <div ref="sketch" class="sketch" ></div>
+    <div class="sketchModeContainer">
+        <div class="pointSizeContainer">
+            <button class="mainMenuButton" @click="changePointSize10">10</button>
+            <button class="mainMenuButton" @click="changePointSize20">20</button>
+            <button class="mainMenuButton" @click="changePointSize30">30</button>
+        </div>
+        <div class="sketchContainer">
+            <div ref="sketch" class="sketch" ></div>
+            <button class="mainMenuButton submitSketchButton" @click="submit">Submit</button>
+        </div>
     </div>
-    <button @click="clear">Clear</button>
 </template>
 
 <style>
-    .sketchContainer {
+    .sketchModeContainer {
         display: flex;
-        flex-direction: column;
-        align-items: center;
+        flex-direction: row;
+        align-items: top;
         justify-content: center;
         width: 100%;
     }
 
+    .sketchContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: top;
+    }
+
     .sketch {
         box-shadow: 2px 2px 2px rgb(20, 20, 20);
+    }
+
+    .submitSketchButton {
+        margin-top: 1rem;
+        width: 13rem;
+    }
+
+    .pointSizeContainer {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
     }
 </style>
