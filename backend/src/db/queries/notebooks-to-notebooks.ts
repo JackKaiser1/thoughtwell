6a5a2726bb4988e8a6863b1bfa8841d5c578e7f8
jackkaiser1 <jackkaiser78@gmail.com>
2026-09-14 12:57:16 -0400

    feat(backend): add sketch type to getChildren query

--- backend/src/db/queries/notebooks-to-notebooks.ts
+++ backend/src/db/queries/notebooks-to-notebooks.ts
@@ -1,6 +1,6 @@
 import { dbClient } from "../index.js";
 import { eq } from "drizzle-orm";
-import { type NotebooksToNotebooksRecord, PagesToNotebooksRecord, notebooksToNotebooks, pages, pagesToNotebooks } from "../schema.js";
+import { type NotebooksToNotebooksRecord, PagesToNotebooksRecord, SketchesToNotebooksRecord, notebooksToNotebooks, pages, pagesToNotebooks, sketchesToNotebooks } from "../schema.js";
 import { exceptAll, unionAll } from 'drizzle-orm/pg-core'
 
 export type NotebooksToNotebooksQuery = Omit<NotebooksToNotebooksRecord, "id" | "createdAt" | "updatedAt">;
@@ -8,6 +8,7 @@ export type NotebooksToNotebooksQuery = Omit<NotebooksToNotebooksRecord, "id" | 
 export type TopLevelChildren = {
     pageChildren: PagesToNotebooksRecord[],
     notebookChildren: NotebooksToNotebooksRecord[],
+    sketchChildren: SketchesToNotebooksRecord[]
 }
 
 export async function createNotebooksToNotebooks(client: dbClient, queryData: NotebooksToNotebooksQuery) {
@@ -20,18 +21,23 @@ export async function createNotebooksToNotebooks(client: dbClient, queryData: No
 }
 
 export async function getChildren(client: dbClient, notebookId: string): Promise<TopLevelChildren> {
-    const childNotebookRecords = await client 
+    const pageNotebookRecords = await client 
                                             .select()
                                             .from(pagesToNotebooks)
                                             .where(eq(pagesToNotebooks.parentNotebookId, notebookId));
     const notebookNotebooksRecords = await client 
-                                                .select()
-                                                .from(notebooksToNotebooks)
-                                                .where(eq(notebooksToNotebooks.parentNotebookId, notebookId));
+                                            .select()
+                                            .from(notebooksToNotebooks)
+                                            .where(eq(notebooksToNotebooks.parentNotebookId, notebookId));
+    const sketchNotebookRecords = await client 
+                                            .select()
+                                            .from(sketchesToNotebooks)
+                                            .where(eq(sketchesToNotebooks.parentNotebookId, notebookId));
 
     return {
-        pageChildren: childNotebookRecords,
+        pageChildren: pageNotebookRecords,
         notebookChildren: notebookNotebooksRecords,
+        sketchChildren: sketchNotebookRecords
     };
 }
 
